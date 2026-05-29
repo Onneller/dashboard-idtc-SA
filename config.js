f17d8dbea144d3b5d853fe441ff3960da9f0b17a
Onneller <90772636+Onneller@users.noreply.github.com>
2026-05-29 04:59:29 -0500

    Update config.js
    AVALAIBLE 404

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby8GDnSLLUFTCVmbwszZMQocMqkOmBm9fnV_ViriR2vJMJNt1DxniX6BNI8ZalJkmk6qQ/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzxazQfBR7HL7oD6fKL6UVNSLCjjFQWa0x_eI5ybHlo1zNUNmQkprhsVBp89zFVIECoQg/exec"
 };
