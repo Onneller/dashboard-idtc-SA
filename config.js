07c5a9a4a5ffeb7e14c8b9eb31283e56b712314c
Onneller <90772636+Onneller@users.noreply.github.com>
2026-05-29 08:28:52 -0500

    Update config.js
    configurar privacidad

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/a/macros/transitocesar.gov.co/s/AKfycbwQtYhS2cqkxsz_GBDtycUxjNb8bxJFqvmVBkZt6D4_0m8ZXFwTF5v11Gi4fR1l2F3qTQ/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/.../exec",
+    // ========================================================
+    // INTERRUPTOR DE SEGURIDAD MAESTRO
+    // true  = Modo Privado (Activa lista de correos y camuflaje 404)
+    // false = Modo Público (Cualquiera en internet puede ver las cifras)
+    // ========================================================
+    MODO_PRIVADO: false 
 };
