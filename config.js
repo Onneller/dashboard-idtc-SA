22a924e83c6ab0615ec9fb71410e1571e8b2d805
Onneller <90772636+Onneller@users.noreply.github.com>
2026-05-29 08:20:04 -0500

    Update config.js
    actualizada implementacion 1

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzxazQfBR7HL7oD6fKL6UVNSLCjjFQWa0x_eI5ybHlo1zNUNmQkprhsVBp89zFVIECoQg/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/a/macros/transitocesar.gov.co/s/AKfycbwQtYhS2cqkxsz_GBDtycUxjNb8bxJFqvmVBkZt6D4_0m8ZXFwTF5v11Gi4fR1l2F3qTQ/exec"
 };
