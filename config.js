ae74225e4aebb68c06f3c377705c75d1e48b99e9
Onneller <90772636+Onneller@users.noreply.github.com>
2026-05-29 08:29:22 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/.../exec",
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzxazQfBR7HL7oD6fKL6UVNSLCjjFQWa0x_eI5ybHlo1zNUNmQkprhsVBp89zFVIECoQg/exec",
     // ========================================================
     // INTERRUPTOR DE SEGURIDAD MAESTRO
     // true  = Modo Privado (Activa lista de correos y camuflaje 404)
